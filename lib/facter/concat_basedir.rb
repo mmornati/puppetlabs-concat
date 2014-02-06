7d355b79b3bcd312a78656ca8e30675e5925522f
Marco Mornati <ilmorna@gmail.com>
2014-02-06 11:50:26 +0100

    Moved concat basedir out of /var (for mountpoint not executable)

--- lib/facter/concat_basedir.rb
+++ lib/facter/concat_basedir.rb
@@ -3,9 +3,12 @@
 # A custom fact that sets the default location for fragments
 #
 # "${::vardir}/concat/"
+# Moved concat basedir out from /var mountpoint, to prevent problem in
+# environments where /var is not executable mountpoint
 #
 Facter.add("concat_basedir") do
   setcode do
-    File.join(Puppet[:vardir],"concat")
+    #File.join(Puppet[:vardir],"concat")
+    File.join("/tmp","concat")
   end
 end
